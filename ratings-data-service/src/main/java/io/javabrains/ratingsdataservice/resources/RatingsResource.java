63ff24617d19e87b316549a435ded0659430973b
Roumil Sharma <36966827+RoumilSharma@users.noreply.github.com>
2019-04-15 21:48:49 +0530

    Add files via upload

---
+++ ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,29 @@
+package io.javabrains.ratingsdataservice.resources;
+
+import io.javabrains.ratingsdataservice.model.Rating;
+import io.javabrains.ratingsdataservice.model.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import java.util.*;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+      	List<Rating> ratings = Arrays.asList(new Rating("100",4),new Rating("200",3));
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+
+}
